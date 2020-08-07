5c61ca5d95105e40d58ad3a44a2319dbf756588b
sokthearymao <68365381+sokthearymao@users.noreply.github.com>
2020-08-07 14:24:24 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv/config')
+
+//MIDDLEWARES
+app.use(cors())
+app.use(bodyParser.json())
+
+//Import Routes
+const postsRoute = require('./routes/posts')
+
+app.use('/posts', postsRoute)
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('We are on home')
+})
+
+//Connect To DB
+mongoose.set('useUnifiedTopology', true)
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true },
+    () => console.log('DB has connected successfully!')
+)
+
+//How do we start listening to the server
+app.listen(3000)
